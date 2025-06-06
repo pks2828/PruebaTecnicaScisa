2f28ea47e11950f4b3417b5b41c4f3e828f7f9ce
pks2828 <danieldoriamoncada28@gmail.com>
2025-06-05 18:35:13 -0600

    [ADD] partials

---
+++ Views/Pokemon/_DetailModalPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiPokemonApp.Views.Pokemon
+{
+    public class _DetailModalPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Pokemon/_PaginationPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiPokemonApp.Views.Pokemon
+{
+    public class _PaginationPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
